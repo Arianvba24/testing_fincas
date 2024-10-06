2ad894b1f2701de849aa09cb30ac769d3e426894
Arianvba24 <153762786+Arianvba24@users.noreply.github.com>
2024-10-07 00:22:32 +0200

    Update testing2.py

--- testing2.py
+++ testing2.py
@@ -5,7 +5,7 @@ import re
 def cosas():
     browser = mechanicalsoup.StatefulBrowser()
 
-    browser.add_headers({
+    browser.session.headers.update({
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
         "Accept-Language": "es-ES,es;q=0.9",
